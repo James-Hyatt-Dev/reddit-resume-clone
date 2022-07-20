5ec4807d19bc77755d57bdecb1a5a42c163516cf
James Hyatt <jameshyattdevelopment@JamessMacStudio.lan>
2022-07-20 15:14:50 -0400

    adds conditional logic to that if the user starts to type something to create a post, a Body and a Subreddit input fields appear for them to fill in

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -1,16 +1,34 @@
 import { useSession } from 'next-auth/react'
 import React from 'react'
 import Avatar from './Avatar'
+import { LinkIcon, PhotographIcon } from '@heroicons/react/outline'
+import { useForm } from 'react-hook-form'
+
+type FormData ={
+    postTitle :string
+    postBody: string
+    postImage: string
+    subreddit: string
+}
 
 function Postbox() {
     const {data: session} = useSession()
+    const {
+        register,
+        setValue,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<FormData>()
 
   return (
-    <form>
+    <form className='sticky top-16 z-50 bg-white border rounded-md border-gray-300 p-2' >
+
         <div className='flex items-center space-x-3'>
             {/* Avatar */}
             <Avatar />
             <input 
+                {...register('postTitle', { required: true })}
                 disabled={!session}
                 className='rounded-md flex-1 bg-gray-50 p-2 pl-5 outline-none'
                 type="text" 
@@ -18,9 +36,38 @@ function Postbox() {
                     session ? 'Create a post by entering a title' : 'Sign in to post'
                 }
             />
+
+            <PhotographIcon className={`h-6 text-gray-300 cursor-pointer`} />
+            <LinkIcon className='h-6 text-gray-300' />
+
         </div>
+
+        { !!watch('postTitle') && (
+            <div className='flex flex-col py-2'>
+            {/* Body */}
+                <div className='flex items-center px-2'>
+                    <p className='min-w-[90px]'>Body:</p>
+                    <input className='m-2 flex-1 bg-blue-50 p-2 outline-none' 
+                        {...register('postBody')} 
+                        type='text' 
+                        placeholder='text (optional)' 
+                    />
+                </div>
+
+                <div className='flex items-center px-2'>
+                    <p className='min-w-[90px]'>Subreddit:</p>
+                    <input className='m-2 flex-1 bg-blue-50 p-2 outline-none' 
+                        {...register('subreddit')} 
+                        type='text' 
+                        placeholder='i.e. nextjs' 
+                    />
+                </div>
+            </div>
+        ) }
     </form>
   )
 }
 
 export default Postbox
+
+
